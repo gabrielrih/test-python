5ea0b44dffd536367411add02640ac7c589f0bc6
Gabriel Richter <gabriel.richter@ambevtech.com.br>
2022-11-10 00:12:08 -0300

    Add not working test

---
+++ rabbitmq/test-not-working/unit/model.py
@@ -0,0 +1,10 @@
+from resources.rabbit_cfg import RabbitCfg
+
+
+class Connection:
+
+    def __init__(self, *kwargs):
+        return 'connection'
+
+    def channel(self):
+        return 'channel'
---
+++ rabbitmq/test-not-working/unit/resources/test_rabbit.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import unittest
+import pytest
+
+from unittest import mock
+
+HERE = os.path.dirname(os.path.realpath(__file__))
+root_path = f'{HERE}/../../..'
+sys.path.insert(0, root_path)
+
+from resources.rabbit import Rabbit
+from test.unit import model
+
+class TestRabbit(unittest.TestCase):
+    def setUp(self):
+        self.get_environment_variable = mock.patch('os.getenv', lambda *x: 'fake_variable')
+        self.startMock()
+
+    def startMock(self):
+        self.get_environment_variable.start()
+
+    def stopMock(self):
+         self.get_environment_variable.stop()
+
+    @mock.patch('__open_connection')
+    def test_open_channel(self, mock_open_conn):
+        # Given
+        rabbit = Rabbit()
+        mock_open_conn.return_value = model.Connection()
+
+        # When
+        channel = rabbit.open_channel()
+
+        # Then
+        self.assertEqual(None, channel)
